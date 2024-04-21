b8b124f9bfabe8ba0eb4541aac1dd111a6cde23b
royman.dev <royman.dev@gmail.com>
2024-04-22 04:13:06 +0700

    feat: change main page menu

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,19 +1,51 @@
 import { PaperTab } from '@/components/utils/PaperTab';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 function MainLayout() {
   return (
     <PaperTab>
       <PaperTab.Screen
         name="index"
-        options={{ title: 'Home', tabBarIcon: 'home' }}
-      />
-      <PaperTab.Screen
-        name="transactions"
-        options={{ title: 'Trans.', tabBarIcon: 'book' }}
+        options={{
+          title: 'Trans.',
+          tabBarIcon: ({ focused, color }) => (
+            <MaterialCommunityIcons
+              name={focused ? 'notebook' : 'notebook-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
       />
       <PaperTab.Screen
         name="statistics"
-        options={{ title: 'Stats', tabBarIcon: 'chart-bar' }}
+        options={{
+          title: 'Stats',
+          tabBarIcon: ({ focused, color }) => (
+            <MaterialCommunityIcons
+              name={focused ? 'chart-box' : 'chart-box-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <PaperTab.Screen
+        name="accounts"
+        options={{
+          title: 'Accounts',
+          tabBarIcon: ({ focused, color }) => (
+            <MaterialCommunityIcons
+              name={
+                focused
+                  ? 'credit-card-multiple'
+                  : 'credit-card-multiple-outline'
+              }
+              color={color}
+              size={24}
+            />
+          ),
+        }}
       />
       <PaperTab.Screen
         name="others"
