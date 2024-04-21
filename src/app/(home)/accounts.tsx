b1c5fabf3601bdb5e5144e5fe918a6a5c2e184ae
royman.dev <royman.dev@gmail.com>
2024-04-22 04:13:06 +0700

    feat: change main page menu

--- src/app/(home)/accounts.tsx
+++ src/app/(home)/accounts.tsx
@@ -4,7 +4,7 @@ import { Button } from 'react-native-paper';
 export default function TransactionsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button mode="outlined">Transactions</Button>
+      <Button mode="outlined">Accounts</Button>
     </View>
   );
 }
