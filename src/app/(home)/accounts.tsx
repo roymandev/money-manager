c01004e88aa277349bb96541470e5f2df6cd672f
royman.dev <royman.dev@gmail.com>
2024-04-23 03:24:05 +0700

    feat(accounts): add account list

--- src/app/(home)/accounts.tsx
+++ src/app/(home)/accounts.tsx
@@ -1,9 +1,14 @@
-import { Appbar } from 'react-native-paper';
+import { SectionList, View } from 'react-native';
+import { Appbar, List, Text, useTheme } from 'react-native-paper';
 
+import Currency from '@/components/Currency';
 import PaperStackHeader from '@/components/PaperStackHeader';
 import TotalHeader from '@/components/TotalHeader';
+import { DUMMY_ACCOUNTS } from '@/modules/accounts/dummyData';
 
 export default function TransactionsScreen() {
+  const theme = useTheme();
+
   return (
     <>
       <PaperStackHeader options={{ title: 'Accounts' }}>
@@ -27,6 +32,52 @@ export default function TransactionsScreen() {
           },
         ]}
       />
+
+      <List.Section style={{ flex: 1, marginVertical: 0 }}>
+        <SectionList
+          sections={DUMMY_ACCOUNTS}
+          keyExtractor={({ id }) => String(id)}
+          // style={{ flex: 1 }}
+          renderSectionHeader={({ section: { title, data } }) => {
+            const total = data.reduce((acc, item) => {
+              return acc + item.balance;
+            }, 0);
+
+            return (
+              <View
+                style={{
+                  flexDirection: 'row',
+                  paddingBottom: 8,
+                  paddingTop: 24,
+                  paddingLeft: 16,
+                  paddingRight: 24,
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Text>{title}</Text>
+                <Currency amount={total} />
+              </View>
+            );
+          }}
+          renderItem={({ item }) => (
+            <List.Item
+              key={item.id}
+              title={item.name}
+              onPress={() => console.log('Pressed')}
+              style={{
+                backgroundColor: theme.colors.elevation.level1,
+              }}
+              right={() => (
+                <Currency
+                  amount={item.balance}
+                  withSymbol
+                  style={{ alignSelf: 'center' }}
+                />
+              )}
+            />
+          )}
+        />
+      </List.Section>
     </>
   );
 }
