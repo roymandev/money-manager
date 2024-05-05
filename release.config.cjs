cd55de2f184fe458450d65bad0dcec882e2e5578
royman.dev <royman.dev@gmail.com>
2024-05-05 08:45:26 +0700

    ci: configure semantic release

---
+++ release.config.cjs
@@ -0,0 +1,18 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
