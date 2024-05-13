0233b9bd3b039ffbdf8cc422c8e9e82b42abeed2
Royman <royman.dev@gmail.com>
2024-05-14 01:59:34 +0700

    feat(transaction): add edit page (#11)

--- src/app/transaction/add.tsx
+++ src/app/transaction/add.tsx
@@ -1,126 +1,24 @@
-import { FAB, SegmentedButtons } from 'react-native-paper';
-
 import { useRouter } from 'expo-router';
-import { Controller, useForm } from 'react-hook-form';
-import { ScrollView } from 'react-native-gesture-handler';
-import { Input } from 'valibot';
 
-import ButtonDatePicker from '@/components/ButtonDatePicker';
-import CurrencyInput from '@/components/CurrencyInput';
 import PaperStackHeader from '@/components/PaperStackHeader';
-import TextInput from '@/components/TextInput';
+import TransactionForm from '@/modules/transactions/TransactionForm';
 import { useTransactionsAdd } from '@/modules/transactions/queries';
-import { schemaTransactionInsert } from '@/schema';
-import { valibotResolver } from '@hookform/resolvers/valibot';
+import { TTransactionInput } from '@/modules/transactions/types';
 
 function AddTransactionPage() {
   const router = useRouter();
   const mutation = useTransactionsAdd();
 
-  const { control, handleSubmit, setFocus } = useForm<
-    Input<typeof schemaTransactionInsert>
-  >({
-    resolver: valibotResolver(schemaTransactionInsert),
-    shouldFocusError: false,
-    defaultValues: {
-      type: 'expense',
-      amount: 0,
-      date: new Date().toISOString(),
-      category: '',
-    },
-  });
-
-  const handleOnSubmit = handleSubmit(async (data) => {
+  const handleOnSubmit = async (data: TTransactionInput) => {
     await mutation.mutateAsync(data);
     router.back();
-  });
+  };
 
   return (
     <>
       <PaperStackHeader options={{ title: 'Add Transaction' }} withBackButton />
 
-      <ScrollView
-        contentContainerStyle={{
-          paddingBottom: 88,
-          paddingHorizontal: 16,
-          gap: 16,
-        }}
-      >
-        <Controller
-          control={control}
-          name="type"
-          render={({ field: { value, onChange } }) => (
-            <SegmentedButtons
-              value={value}
-              onValueChange={onChange}
-              buttons={[
-                {
-                  value: 'expense',
-                  label: 'Expense',
-                },
-                {
-                  value: 'income',
-                  label: 'Income',
-                },
-              ]}
-            />
-          )}
-        />
-
-        <Controller
-          control={control}
-          name="date"
-          render={({ field: { onChange, ...rest }, fieldState }) => (
-            <ButtonDatePicker
-              label="Date"
-              onChange={(value) => {
-                onChange(value);
-                setTimeout(() => setFocus('amount'), 200);
-              }}
-              error={fieldState.error?.message}
-              {...rest}
-            />
-          )}
-        />
-
-        <Controller
-          control={control}
-          name="category"
-          render={({ field: { onChange, ...rest }, fieldState }) => (
-            <TextInput
-              label="Category"
-              mode="outlined"
-              onChangeText={onChange}
-              error={fieldState.error?.message}
-              {...rest}
-            />
-          )}
-        />
-
-        <Controller
-          control={control}
-          name="amount"
-          render={({ field: { onChange, ...rest }, fieldState }) => (
-            <CurrencyInput
-              label="Amount"
-              onValueChange={onChange}
-              error={fieldState.error?.message}
-              {...rest}
-            />
-          )}
-        />
-      </ScrollView>
-
-      <FAB
-        icon="content-save"
-        label="Save"
-        style={{
-          position: 'absolute',
-          bottom: 16,
-          right: 16,
-        }}
-        onPress={handleOnSubmit}
-      />
+      <TransactionForm onSubmit={handleOnSubmit} />
     </>
   );
 }
